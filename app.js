ff2291efc8a49748c75fbb25bd864dd1d52bb22f
2lmf <2lmf.info@gmail.com>
2026-02-28 22:36:15 +0100

    Fix: SharkTuner v2.1.1 - Forced Cache Refresh, Full Chord/Scale Database, Inverted Strings

--- app.js
+++ app.js
@@ -89,15 +89,19 @@ function updateTuner() {
     requestAnimationFrame(updateTuner);
 }
 
-// --- V2.1 LIBRARIES: CHORDS & SCALES ---
+// --- V2.1.1 LIBRARIES: EXPANDED CHORDS & SCALES ---
 
 const GUITAR_CHORDS = {
     "C": {
         "major": { name: "C Dur", notes: "C E G", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 2 }, { s: 2, f: 1 }] },
-        "minor": { name: "C Mol", notes: "C Eb G", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 5 }, { s: 3, f: 5 }, { s: 2, f: 4 }] },
+        "minor": { name: "C Mol", notes: "C Eb G", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 5 }, { s: 3, f: 5 }, { s: 2, f: 4 }, { s: 1, f: 3 }] },
         "7": { name: "C7", notes: "C E G Bb", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 2 }, { s: 3, f: 3 }, { s: 2, f: 1 }] },
         "maj7": { name: "Cmaj7", notes: "C E G B", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 2 }, { s: 3, f: 0 }, { s: 2, f: 0 }] },
         "m7": { name: "Cm7", notes: "C Eb G Bb", positions: [{ s: 5, f: 3, r: true }, { s: 4, f: 5 }, { s: 3, f: 3 }, { s: 2, f: 4 }] }
+    },
+    "C#": {
+        "major": { name: "C# Dur", notes: "C# F G#", positions: [{ s: 5, f: 4, r: true }, { s: 4, f: 6 }, { s: 3, f: 6 }, { s: 2, f: 6 }] },
+        "minor": { name: "C# Mol", notes: "C# E G#", positions: [{ s: 5, f: 4, r: true }, { s: 4, f: 6 }, { s: 3, f: 6 }, { s: 2, f: 5 }] }
     },
     "D": {
         "major": { name: "D Dur", notes: "D F# A", positions: [{ s: 4, f: 0, r: true }, { s: 3, f: 2 }, { s: 2, f: 3 }, { s: 1, f: 2 }] },
@@ -106,19 +110,34 @@ const GUITAR_CHORDS = {
         "maj7": { name: "Dmaj7", notes: "D F# A C#", positions: [{ s: 4, f: 0, r: true }, { s: 3, f: 2 }, { s: 2, f: 2 }, { s: 1, f: 2 }] },
         "m7": { name: "Dm7", notes: "D F A C", positions: [{ s: 4, f: 0, r: true }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 1 }] }
     },
+    "D#": {
+        "major": { name: "D# Dur", notes: "D# G A#", positions: [{ s: 5, f: 6, r: true }, { s: 4, f: 8 }, { s: 3, f: 8 }, { s: 2, f: 8 }] }
+    },
     "E": {
         "major": { name: "E Dur", notes: "E G# B", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }, { s: 4, f: 2 }, { s: 3, f: 1 }] },
         "minor": { name: "E Mol", notes: "E G B", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }, { s: 4, f: 2 }] },
-        "7": { name: "E7", notes: "E G# B D", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }, { s: 3, f: 1 }] },
+        "7": { name: "E7", notes: "E G# B D", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 3, f: 1 }, { s: 2, f: 0 }] },
         "maj7": { name: "Emaj7", notes: "E G# B D#", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }, { s: 4, f: 1 }, { s: 3, f: 1 }] },
         "m7": { name: "Em7", notes: "E G B D", positions: [{ s: 6, f: 0, r: true }, { s: 5, f: 2 }] }
     },
+    "F": {
+        "major": { name: "F Dur", notes: "F A C", positions: [{ s: 6, f: 1, r: true }, { s: 5, f: 3 }, { s: 4, f: 3 }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 1 }] },
+        "minor": { name: "F Mol", notes: "F Ab C", positions: [{ s: 6, f: 1, r: true }, { s: 5, f: 3 }, { s: 4, f: 3 }, { s: 3, f: 1 }, { s: 2, f: 1 }, { s: 1, f: 1 }] },
+        "7": { name: "F7", notes: "F A C Eb", positions: [{ s: 6, f: 1, r: true }, { s: 5, f: 3 }, { s: 4, f: 1 }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 1 }] },
+        "maj7": { name: "Fmaj7", notes: "F A C E", positions: [{ s: 4, f: 3, r: true }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 0 }] }
+    },
+    "F#": {
+        "major": { name: "F# Dur", notes: "F# A# C#", positions: [{ s: 6, f: 2, r: true }, { s: 5, f: 4 }, { s: 4, f: 4 }, { s: 3, f: 3 }, { s: 2, f: 2 }, { s: 1, f: 2 }] }
+    },
     "G": {
-        "major": { name: "G Dur", notes: "G B D", positions: [{ s: 6, f: 3, r: true }, { s: 5, f: 2 }, { s: 1, f: 3 }] },
+        "major": { name: "G Dur", notes: "G B D", positions: [{ s: 6, f: 3, r: true }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 3, f: 0 }, { s: 2, f: 0 }, { s: 1, f: 3 }] },
         "minor": { name: "G Mol", notes: "G Bb D", positions: [{ s: 6, f: 3, r: true }, { s: 5, f: 5 }, { s: 4, f: 5 }, { s: 3, f: 3 }, { s: 2, f: 3 }, { s: 1, f: 3 }] },
-        "7": { name: "G7", notes: "G B D F", positions: [{ s: 6, f: 3, r: true }, { s: 5, f: 2 }, { s: 1, f: 1 }] },
+        "7": { name: "G7", notes: "G B D F", positions: [{ s: 6, f: 3, r: true }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 3, f: 0 }, { s: 2, f: 0 }, { s: 1, f: 1 }] },
         "maj7": { name: "Gmaj7", notes: "G B D F#", positions: [{ s: 6, f: 3, r: true }, { s: 1, f: 2 }] },
         "m7": { name: "Gm7", notes: "G Bb D F", positions: [{ s: 6, f: 3, r: true }, { s: 3, f: 3 }, { s: 2, f: 3 }, { s: 1, f: 3 }] }
+    },
+    "G#": {
+        "major": { name: "G# Dur", notes: "G# C D#", positions: [{ s: 6, f: 4, r: true }, { s: 5, f: 6 }, { s: 4, f: 6 }, { s: 3, f: 5 }, { s: 2, f: 4 }, { s: 1, f: 4 }] }
     },
     "A": {
         "major": { name: "A Dur", notes: "A C# E", positions: [{ s: 5, f: 0, r: true }, { s: 4, f: 2 }, { s: 3, f: 2 }, { s: 2, f: 2 }] },
@@ -127,15 +146,15 @@ const GUITAR_CHORDS = {
         "maj7": { name: "Amaj7", notes: "A C# E G#", positions: [{ s: 5, f: 0, r: true }, { s: 4, f: 2 }, { s: 3, f: 1 }, { s: 2, f: 2 }] },
         "m7": { name: "Am7", notes: "A C E G", positions: [{ s: 5, f: 0, r: true }, { s: 4, f: 2 }, { s: 2, f: 1 }] }
     },
-    "F": {
-        "major": { name: "F Dur", notes: "F A C", positions: [{ s: 6, f: 1, r: true }, { s: 5, f: 3 }, { s: 4, f: 3 }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 1 }] },
-        "7": { name: "F7", notes: "F A C Eb", positions: [{ s: 6, f: 1, r: true }, { s: 5, f: 3 }, { s: 4, f: 1 }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 1 }] },
-        "maj7": { name: "Fmaj7", notes: "F A C E", positions: [{ s: 4, f: 3, r: true }, { s: 3, f: 2 }, { s: 2, f: 1 }, { s: 1, f: 0 }] }
+    "A#": {
+        "major": { name: "A# Dur", notes: "A# D F", positions: [{ s: 5, f: 1, r: true }, { s: 4, f: 3 }, { s: 3, f: 3 }, { s: 2, f: 3 }] }
     },
     "B": {
         "major": { name: "B Dur", notes: "B D# F#", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 4 }, { s: 3, f: 4 }, { s: 2, f: 4 }] },
         "minor": { name: "B Mol", notes: "B D F#", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 4 }, { s: 3, f: 4 }, { s: 2, f: 3 }] },
-        "7": { name: "B7", notes: "B D# F# A", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 1 }, { s: 3, f: 2 }, { s: 1, f: 2 }] }
+        "7": { name: "B7", notes: "B D# F# A", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 1 }, { s: 3, f: 2 }, { s: 2, f: 0 }, { s: 1, f: 2 }] },
+        "maj7": { name: "Bmaj7", notes: "B D# F# A#", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 4 }, { s: 3, f: 3 }, { s: 2, f: 4 }] },
+        "m7": { name: "Bm7", notes: "B D F# A", positions: [{ s: 5, f: 2, r: true }, { s: 4, f: 4 }, { s: 3, f: 2 }, { s: 2, f: 3 }] }
     }
 };
 
@@ -164,6 +183,11 @@ const GUITAR_SCALES = {
         "major_penta": [{ s: 6, f: 0, r: true }, { s: 6, f: 2 }, { s: 5, f: 0 }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 4, f: 2 }, { s: 3, f: 1 }, { s: 3, f: 4 }, { s: 2, f: 0 }, { s: 2, f: 2 }, { s: 1, f: 0 }, { s: 1, f: 2 }],
         "minor_penta": [{ s: 6, f: 0, r: true }, { s: 6, f: 3 }, { s: 5, f: 0 }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 4, f: 2 }, { s: 3, f: 0 }, { s: 3, f: 2 }, { s: 2, f: 0 }, { s: 2, f: 3 }, { s: 1, f: 0 }, { s: 1, f: 3 }],
         "blues": [{ s: 6, f: 0, r: true }, { s: 6, f: 3 }, { s: 5, f: 0 }, { s: 5, f: 1 }, { s: 5, f: 2 }, { s: 4, f: 0 }, { s: 4, f: 2 }, { s: 3, f: 0 }, { s: 3, f: 2 }, { s: 3, f: 3 }, { s: 2, f: 0 }, { s: 1, f: 0 }]
+    },
+    "B": {
+        "major_penta": [{ s: 5, f: 2, r: true }, { s: 5, f: 4 }, { s: 4, f: 1 }, { s: 4, f: 4 }, { s: 3, f: 1 }, { s: 3, f: 4 }, { s: 2, f: 2 }, { s: 2, f: 4 }, { s: 1, f: 2 }, { s: 1, f: 4 }],
+        "minor_penta": [{ s: 6, f: 7, r: true }, { s: 6, f: 10 }, { s: 5, f: 7 }, { s: 5, f: 9 }, { s: 4, f: 7 }, { s: 4, f: 9 }, { s: 3, f: 7 }, { s: 3, f: 9 }, { s: 2, f: 7 }, { s: 2, f: 10 }, { s: 1, f: 7 }, { s: 1, f: 10 }],
+        "blues": [{ s: 6, f: 7, r: true }, { s: 6, f: 10 }, { s: 5, f: 7 }, { s: 5, f: 8 }, { s: 5, f: 9 }, { s: 4, f: 7 }, { s: 4, f: 9 }, { s: 3, f: 7 }, { s: 3, f: 9 }, { s: 3, f: 10 }, { s: 2, f: 7 }, { s: 1, f: 7 }]
     }
 };
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sharktuner-v1.5';
+const CACHE_NAME = 'sharktuner-v2.1';
 const ASSETS = [
     './',
     './index.html',
